6f5f35ecd531d4f92aab267371afff748db656d0
krytical <johnnie-l@Live.ca>
2014-09-16 22:07:42 -0700

    Resolved conflicts

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -1,19 +1,13 @@
 package ca.ubc.cpsc310.gitlab.client;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 
 import ca.ubc.cpsc310.gitlab.client.products.ProductItem;
 import ca.ubc.cpsc310.gitlab.client.user.IUser;
 import ca.ubc.cpsc310.gitlab.client.user.User;
-=======
-import java.util.List;
-
 import ca.ubc.cpsc310.gitlab.client.service.LoadUsersService;
 import ca.ubc.cpsc310.gitlab.client.service.LoadUsersServiceAsync;
-import ca.ubc.cpsc310.gitlab.client.user.IUser;
->>>>>>> async
 import ca.ubc.cpsc310.gitlab.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -46,12 +40,9 @@ public class GitLab implements EntryPoint {
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
 
-<<<<<<< HEAD
 	private final FlexTable flexTable = new FlexTable();
-=======
 
 	final LoadUsersServiceAsync service = GWT.create(LoadUsersService.class);
->>>>>>> async
 
 	/**
 	 * This is the entry point method.
@@ -73,7 +64,6 @@ public class GitLab implements EntryPoint {
 				
 			}});
 	
-<<<<<<< HEAD
 
 		
 	}
@@ -115,7 +105,5 @@ public class GitLab implements EntryPoint {
 			
 			flexTable.setText(i+1,3,String.valueOf(user.getWishList().size()));
 		}
-=======
->>>>>>> async
 	}
 }
